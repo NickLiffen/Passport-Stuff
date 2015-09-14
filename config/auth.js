e3557a872f8eb4f72d71de6feb5897770d8fe073
Nick Liffen <UP653591@myport.ac.uk>
2015-09-14 10:55:34 +0100

    make it better

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '150885658588539', // your App ID
         'clientSecret'  : '8c504f83ffb14f860301e51c363dcb4a', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://passportlocal.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'bhmo1kmPWyEySI9ejpezBGEha',
         'consumerSecret'    : 'NcKp5kfLfrwwd0mySWSwbLaCpDMfDj4HXKKDqyJvMlljBEGwRx',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://passportlocal.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '734495668730-63deu83o9c8727ls3ul3amh7u3jvk30q.apps.googleusercontent.com',
         'clientSecret'  : 'fHz-FPgSHAen5VqkJyQZdqGe',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://passportlocal.herokuapp.com/auth/google/callback'
     }
 
 };
